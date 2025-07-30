ca5b6d72a6c266f93475c710711386caa02afe6a
Miran Shaikh <114760811+Undead6687@users.noreply.github.com>
2025-07-30 19:59:44 +0100

    Update start_zap.bat to start ZAP
    Updated batch file to start ZAP in background and removed the api upload mistake

--- start_zap.bat
+++ start_zap.bat
@@ -1,5 +1,5 @@
 @echo off
 REM This script starts the ZAP daemon in the background
 cd /d "C:\Program Files\ZAP\Zed Attack Proxy\"
-start zap.bat -daemon -host localhost -port 8080 -config api.key=oclr1pjo9dpeslpgu3pd676p97
+start zap.bat -daemon -host localhost -port 8080 -config api.key=***API_KEY_HERE***
 exit
