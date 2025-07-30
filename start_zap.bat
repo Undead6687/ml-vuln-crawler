f18bfc73f033b28c519e5f07ea568523e5d8f6ad
Miran Shaikh <114760811+Undead6687@users.noreply.github.com>
2025-07-30 19:56:10 +0100

    Update start_zap.bat to start ZAP in new terminal from background

--- start_zap.bat
+++ start_zap.bat
@@ -1,12 +1,5 @@
 @echo off
-echo Starting ZAP in daemon mode...
-echo This will start ZAP without the GUI for API access only
-echo.
-echo To start ZAP daemon:
-cd "C:\Program Files\ZAP\Zed Attack Proxy"
-zap.bat -daemon -host localhost -port 8080 -config api.disablekey=true
-echo.
-echo ZAP daemon should now be running on http://localhost:8080
-echo You can test it by opening: http://localhost:8080 in your browser
-echo.
-pause
+REM This script starts the ZAP daemon in the background
+cd /d "C:\Program Files\ZAP\Zed Attack Proxy\"
+start zap.bat -daemon -host localhost -port 8080 -config api.key=oclr1pjo9dpeslpgu3pd676p97
+exit
